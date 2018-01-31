9b0f6e980192715dc4eacd8557c42cbf9e5b4a23
james <jjannettski@gmail.com>
2018-01-30 21:11:21 -0500

    blah

--- Solved/client/src/pages/Books/Books.js
+++ Solved/client/src/pages/Books/Books.js
@@ -6,7 +6,7 @@ import API from "../../utils/API";
 import { Col, Row, Container } from "../../components/Grid";
 import { List, ListItem } from "../../components/List";
 import { Input, TextArea, FormBtn } from "../../components/Form";
-// Import other components here
+// Import other components heree
 
 class Books extends React.Component {
   constructor(props) {
