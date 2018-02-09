b4a6e5b77681775159477f170372271571334205
james <jjannettski@gmail.com>
2018-02-08 21:28:17 -0500

    updated display and positioning

--- Solved/client/src/components/SignOut/SignOut.js
+++ Solved/client/src/components/SignOut/SignOut.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import "./Signout.css";
 import { auth } from '../../firebase';
 
 const SignOutButton = () =>
@@ -9,7 +9,7 @@ const SignOutButton = () =>
     // >
     //     Sign Out
     // </button>
-    <button onClick={auth.doSignOut} type="button" className="btn btn-danger" aria-label="Right Align">
+    <button onClick={auth.doSignOut} type="button" className="btn" aria-label="Right Align">
     <span className="glyphicon glyphicon-play glyphicon-align-left" aria-hidden="true"></span><span>Signout</span>
   </button>
 
