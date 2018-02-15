eb7b9f3ef3e4ab4a8ebe9ba7d6a69379dfd5c197
yelnats10 <yelnats10@gmail.com>
2018-02-15 13:53:29 -0500

    updated build fix

--- client/src/components/Form/index.js
+++
@@ -1,9 +0,0 @@
-import Input from "./Input";
-import TextArea from "./TextArea";
-import FormBtn from "./FormBtn";
-
-export {
-    Input,
-    TextArea,
-    FormBtn
-}
--- client/src/constants/routes.js
+++
@@ -1,6 +0,0 @@
-export const SIGN_IN = '/';
-export const ITEMS = '/items';
-export const DASHBOARD = '/dashboard';
-export const DETAIL = '/items/:id';
-export const SIGN_UP = '/signup';
-export const PASSWORDFORGET = '/passwordforget';
